ba855a2b74deb2ad519bd2a842ec8a41a332c5a6
Anuj Kumar <kumar_191851@student.nitw.ac.in>
2021-08-03 15:41:43 +0530

    User Authentication was set up...✔✔

--- Backend/api/server.js
+++ Backend/api/server.js
@@ -2,23 +2,9 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const authRoute = require("./routes/Auth");
 
 dotenv.config();
-
-// const connectDB = async () => {
-//   try {
-//     await mongoose.connect(process.env.MONGO_URL, {
-//       useNewUrlParser: true,
-//       useCreateIndex: true,
-//       useUnifiedTopology: true,
-//       useFindAndModify: false,
-//     });
-//     console.log("MongoDB Conected");
-//   } catch (err) {
-//     console.error(err.message);
-//     process.exit(1);
-//   }
-// };
 
 mongoose
   .connect(process.env.MONGO_URL, {
@@ -30,6 +16,8 @@ mongoose
   .catch((err) => {
     (err) => console.log(err);
   });
+
+app.use("/api/auth", authRoute);
 
 app.listen("3000", () => {
   console.log("Cnnection Established!!");
