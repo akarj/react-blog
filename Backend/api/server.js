901f5d02ab9eee8ace540cbee7348a9afd61b160
Anuj Kumar <kumar_191851@student.nitw.ac.in>
2021-08-03 14:01:28 +0530

    mongo DB Connected...✔✔

--- Backend/api/server.js
+++ Backend/api/server.js
@@ -1,5 +1,35 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGO_URL, {
+//       useNewUrlParser: true,
+//       useCreateIndex: true,
+//       useUnifiedTopology: true,
+//       useFindAndModify: false,
+//     });
+//     console.log("MongoDB Conected");
+//   } catch (err) {
+//     console.error(err.message);
+//     process.exit(1);
+//   }
+// };
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  })
+  .then(console.log("MongoDB Connected!"))
+  .catch((err) => {
+    (err) => console.log(err);
+  });
 
 app.listen("3000", () => {
   console.log("Cnnection Established!!");
