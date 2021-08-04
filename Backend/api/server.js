dc23afe6f65b432c32008e6734d33c09fd11888f
Anuj Kumar <kumar_191851@student.nitw.ac.in>
2021-08-04 11:58:52 +0530

    Category section completed, and image file upload working tested!!✔✔

--- Backend/api/server.js
+++ Backend/api/server.js
@@ -4,7 +4,9 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 // const authRoute = require("./routes/Auth");
 // const usersRoute = require("./routes/Users");
-const postsRoute = require("./routes/Posts");
+// const postsRoute = require("./routes/Posts");
+const categoryRoute = require("./routes/Categories");
+const multer = require("multer");
 
 dotenv.config();
 
@@ -19,9 +21,24 @@ mongoose
     (err) => console.log(err);
   });
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been upload");
+});
+
 // app.use("/api/auth", authRoute);
 // app.use("/api/users", usersRoute);
-app.use("/api/posts", postsRoute);
+// app.use("/api/posts", postsRoute);
+app.use("/api/categories", categoryRoute);
 
 app.listen("3000", () => {
   console.log("Cnnection Established!!");
