129000302099a1efb8888a65fcb6d4357e2d91e8
Anuj Kumar <kumar_191851@student.nitw.ac.in>
2021-08-06 16:14:56 +0530

    Created new connection to mongo DB.

--- Backend/api/server.js
+++ Backend/api/server.js
@@ -9,6 +9,7 @@ const categoryRoute = require("./routes/Categories");
 const multer = require("multer");
 
 dotenv.config();
+app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO_URL, {
@@ -21,24 +22,24 @@ mongoose
     (err) => console.log(err);
   });
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "images");
-  },
-  filename: (req, file, cb) => {
-    cb(null, req.body.name);
-  },
-});
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, "images");
+//   },
+//   filename: (req, file, cb) => {
+//     cb(null, req.body.name);
+//   },
+// });
 
-const upload = multer({ storage: storage });
-app.post("/api/upload", upload.single("file"), (req, res) => {
-  res.status(200).json("File has been upload");
-});
+// const upload = multer({ storage: storage });
+// app.post("/api/upload", upload.single("file"), (req, res) => {
+//   res.status(200).json("File has been upload");
+// });
 
 app.use("/api/auth", authRoute);
-app.use("/api/users", usersRoute);
-app.use("/api/posts", postsRoute);
-app.use("/api/categories", categoryRoute);
+// app.use("/api/users", usersRoute);
+// app.use("/api/posts", postsRoute);
+// app.use("/api/categories", categoryRoute);
 
 app.listen("5000", () => {
   console.log("Connection Established!!");
