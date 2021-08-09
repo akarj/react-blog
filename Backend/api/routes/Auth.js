4073cd7d11933db10587d29a8b2cb54b6edd4e5c
Anuj Kumar <kumar_191851@student.nitw.ac.in>
2021-08-09 18:32:37 +0530

    Found the error as save() in not woring in auth.js.

--- Backend/api/routes/Auth.js
+++ Backend/api/routes/Auth.js
@@ -1,24 +1,26 @@
 const router = require("express").Router();
 const User = require("../models/User");
-// const bcrypt = require("bcrypt");
-
+const bcrypt = require("bcrypt");
+//checking
+router.get("/", (req, res) => {
+  console.log("Auth page for Auth js line 6");
+  res.status(200).json({ message: "auth Worked" });
+});
 //Register
 router.post("/register", async (req, res) => {
+  const salt = await bcrypt.genSalt(10);
+  const hashedPass = await bcrypt.hash(req.body.password, salt);
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: hashedPass,
+  });
+  console.log(newUser);
   try {
-    // const salt = await bcrypt.genSalt(10);
-    // const hashedPass = await bcrypt.hash(req.body.password, salt);
-
-    const newUser = new User({
-      username: req.body.username,
-      email: req.body.email,
-      // password: hashedPass,
-      password: req.body.password,
-    });
-
     const user = await newUser.save();
     res.status(200).json(user);
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json({ message: "auth js line 23", user: newUser });
   }
 });
 
--- Backend/api/server.js
+++ Backend/api/server.js
@@ -22,25 +22,32 @@ mongoose
     (err) => console.log(err);
   });
 
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     cb(null, "images");
-//   },
-//   filename: (req, file, cb) => {
-//     cb(null, req.body.name);
-//   },
-// });
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
 
-// const upload = multer({ storage: storage });
-// app.post("/api/upload", upload.single("file"), (req, res) => {
-//   res.status(200).json("File has been upload");
-// });
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been upload");
+});
 
 app.use("/api/auth", authRoute);
-// app.use("/api/users", usersRoute);
-// app.use("/api/posts", postsRoute);
-// app.use("/api/categories", categoryRoute);
-
+app.use("/api/users", usersRoute);
+app.use("/api/posts", postsRoute);
+app.use("/api/categories", categoryRoute);
+// app.use("/", (req, res) => {
+//   console.log("Home PAge for Server js line 44");
+//   res.status(200).json({ message: "Worked" });
+// });
+app.get("/api", (req, res) => {
+  console.log("Home PAge for Server js line 44");
+  res.status(200).json({ message: "api Worked" });
+});
 app.listen("5000", () => {
   console.log("Connection Established!!");
 });
