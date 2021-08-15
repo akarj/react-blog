8734986fbb5339956fc5be73d6dcd5eb0bf4a8a3
Anuj Kumar <kumar_191851@student.nitw.ac.in>
2021-08-15 22:11:50 +0530

    About and Contact pages added..

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Register from "./pages/register/Register.jsx";
 import Settings from "./pages/settings/Settings.jsx";
 import Single from "./pages/single/Single.jsx";
 import Write from "./pages/write/Write.jsx";
+import Contact from "./pages/contact/Contact";
+import About from "./pages/about/About";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { useContext } from "react";
 import { Context } from "./Context/Context.js";
@@ -18,6 +20,12 @@ function App() {
       <Switch>
         <Route path="/" exact>
           <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/contact">
+          <Contact />
         </Route>
         <Route path="/register">{user ? <Home /> : <Register />}</Route>
         <Route path="/login">{user ? <Home /> : <Login />}</Route>
---
+++ src/pages/contact/Contact.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Contact.scss";
+
+export default function Contact() {
+  return (
+    <div className="contactDiv">
+      <span className="contactUsSpan">Contact US</span>
+    </div>
+  );
+}
